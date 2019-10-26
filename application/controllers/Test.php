6225570f3a060ee5e7afa57e56c638ec4350ecc8
akosicedzkii <martinezcederic@gmail.com>
2019-10-26 09:47:08 +0800

    fix some bugs

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -8,16 +8,8 @@ class Test extends CI_Controller {
 		$this->settings_model->get_settings();  
 		$this->v_counter->insert_visitor();   
 	}
-	public function index()
-	{
-		$data["module_name"] = strtolower($this->router->fetch_class());
-		$data["page"] = strtolower($this->router->fetch_class());
-		$data["about_us"] = $this->db->get("dynamic_settings")->row();
-		$this->load->view('template/header.php',$data);
-		$this->load->view('about_us_view');
-		$this->load->view('template/footer.php',$data);
-	}
-    public function send_contact_us()
+	
+    public function index()
 	{
 		$session = $this->session->userdata('submission');
 
